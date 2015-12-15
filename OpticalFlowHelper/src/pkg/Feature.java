0ba14a91acd45edc4186856b69228e989f6b2fbc
bcompter <bcompter@gmail.com>
2015-12-15 10:07:25 -0500

    Added Java based Optical Flow helper to help formatting results

---
+++ OpticalFlowHelper/src/pkg/Feature.java
@@ -0,0 +1,13 @@
+package pkg;
+
+/**
+ * A basic structure to hold the basics fo a feature
+ * @author brian.compter
+ */
+public class Feature 
+{
+    // Data members
+    int x;
+    int y;
+    
+}  // end class Feature
---
+++ OpticalFlowHelper/src/pkg/FeatureList.java
@@ -0,0 +1,13 @@
+package pkg;
+
+import java.util.ArrayList;
+
+/**
+ * Stores a list of features
+ * @author brian.compter
+ */
+public class FeatureList 
+{
+    // List of features
+    ArrayList <Feature> features = new ArrayList();
+}
